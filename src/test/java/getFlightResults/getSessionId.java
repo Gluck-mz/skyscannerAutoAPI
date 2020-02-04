601040b3297d3b906c8baf6149db87a3bc1a7487
Vysakh Viswanadh <vysakhviswanadhan@gmail.com>
2020-02-05 00:41:29 +1100

    API automation code for Skyscanner

---
+++ src/test/java/getFlightResults/getSessionId.java
@@ -0,0 +1,29 @@
+package getFlightResults;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+
+public class getSessionId {
+
+	@Test
+	public void testResponsecode()
+	{
+		Response resp = get("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/ee9925d6-2327-4c60-ae5f-e6258f685b84");
+		int code = resp.getStatusCode();
+		System.out.println("Status code is "+code);
+		Assert.assertEquals(code, 401);
+	}
+	
+	@Test
+	public void testBody()
+	{
+		Response resp = get("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/ee9925d6-2327-4c60-ae5f-e6258f685b84");
+		String data = resp.asString();
+		System.out.println("Data is "+data);
+		System.out.println("Response time "+resp.getTime());
+	}
+	
+}
